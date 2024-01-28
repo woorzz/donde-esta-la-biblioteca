34acbdfe61c9351f0af66abe1e054968d2c81854
ErwannIUT <156093125+ErwannIUT@users.noreply.github.com>
2024-01-28 12:05:23 +0100

    Update LibraryInit.sql

--- LibraryInit.sql
+++ LibraryInit.sql
@@ -41,11 +41,12 @@ INSERT INTO library VALUES (1, "Bibliothèque Roubaix", "44 Av. Jean Lebas, 5910
 INSERT INTO author VALUES (1, "Alexandre", "Dumas"),
 	(2, "Remy", "Synave"),
 	(3, "Dany", "Capitaine"),
-	(4, "Severine", "Lettrez");
-	
+	(4, "Severine", "Lettrez"),
+	(5, "John Ronald Reuel", "Tolkien");
 
-INSERT INTO book VALUES (1, "Le conte de Monte Cristo", 900, "Aventure", 10, 1),
+INSERT INTO book VALUES (1, "Le conte de Monte Cristo", 900, "Aventure", 9, 1),
 	(2, "Les trois mousquetaires", 300, "Aventure", 9, 1),
-	(3, "Apprendre le Java mais pas sur l'île de Java", 900, "Enseignement", 10, 2),
-	(4, "Le RC Lense, un club pas comme les autres", 900, "Histoire", 10, 4),
-	(5, "La RGPD, une protection contre l 'injustice", 900, "Juridique", 10, 1);
+	(3, "Apprendre le Java mais pas sur l'île de Java", 300, "Enseignement", 10, 2),
+	(4, "Le RC Lense, un club pas comme les autres", 100, "Histoire", 8, 4),
+	(5, "La RGPD, une protection contre l 'injustice", 200, "Juridique", 7, 1),
+	(6, "Le seigneur des anneaux", 1200, "Fantasy", 7, 1);
